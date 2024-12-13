1eec10cc5be161f0dc072e0b323b065533ece985
gabriellob0 <g.oliveiralobo@gmail.com>
2024-12-13 21:16:13 +0000

    feat: add proposal

---
+++ lse_diss/modelling/predict.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer, util
+sentences = ["I'm happy", "I'm full of happiness"]
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+#Compute embedding for both lists
+embedding_1= model.encode(sentences[0], convert_to_tensor=True)
+embedding_2 = model.encode(sentences[1], convert_to_tensor=True)
+
+util.pytorch_cos_sim(embedding_1, embedding_2)
+## tensor([[0.6003]])
--- main.R
+++ main.R
@@ -1,5 +1,16 @@
 source("lse_diss/dataset.R")
+library(dplyr)
+library(tidyr)
 
-patents <- fetch_patents()
+api_fields <- c(
+  "patent_id",
+  "patent_title",
+  "patent_date",
+  "inventors"
+)
 
-saveRDS(patents, file = "data/raw/patents.rds")
+#patents <- fetch_patents(fields = api_fields)
+
+#saveRDS(patents, file = "data/raw/patents.rds")
+
+#patents_tidy <- bind_rows(patents$patents)
