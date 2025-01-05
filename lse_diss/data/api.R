96a6167103b4b6d7e5821735e9db7d30818cedd7
gabriellob0 <g.oliveiralobo@gmail.com>
2025-01-05 17:58:23 +0000

    feat: refactoring

--- lse_diss/data/api.R
+++ lse_diss/data/api.R
@@ -1,9 +1,39 @@
+# PatentsView API Client
+
 library(httr2)
-library(checkmate)
+library(purrr)
 
+# Configuration -----------------------------------------------------------
+ENDPOINTS <- list(
+  patent = list(
+    path = "patent",
+    default_fields = c("patent_id", "patent_title", "patent_date"),
+    id_field = "patent_id"
+  ),
+  citations = list(
+    path = "patent/us_patent_citation",
+    default_fields = c("patent_id", "citation_patent_id", "citation_category"),
+    id_field = "citation_patent_id"
+  ),
+  inventors = list(
+    path = "inventor",
+    default_fields = c("inventor_id", "inventor_lastknown_location"),
+    id_field = "inventor_id"
+  ),
+  locations = list(
+    path = "location",
+    default_fields = c("location_id", "location_name", "location_latitude", "location_longitude"),
+    id_field = "location_id"
+  )
+)
 
-# Helpers -----------------------------------------------------------------
+CONFIG <- list(
+  base_url = "https://search.patentsview.org",
+  rate_limit = 45 / 60, # requests per second
+  max_page_size = 1000
+)
 
+# Core functions ----------------------------------------------------------
 get_api_key <- function() {
   key <- Sys.getenv("PATENTSVIEW_API_KEY")
   if (identical(key, "")) {
@@ -12,23 +42,20 @@ get_api_key <- function() {
   key
 }
 
-create_request <- function(
-    endpoint,
-    api_key,
-    query,
-    fields = NULL,
-    sort = NULL,
-    options = NULL) {
-  params <- list(q = query, f = fields, s = sort, o = options)
-  params <- params[!sapply(params, is.null)]
+create_request <- function(endpoint, query, fields = NULL, sort = NULL,
+                           options = NULL, api_key = get_api_key()) {
+  params <- list(
+    q = query,
+    f = fields,
+    s = sort,
+    o = options
+  )
 
-  req <- request("https://search.patentsview.org") |>
-    req_url_path("/api/v1/") |>
-    req_url_path_append(endpoint) |>
+  request(CONFIG$base_url) |>
+    req_url_path("api", "v1", endpoint) |>
     req_headers("X-Api-Key" = api_key) |>
-    req_throttle(45 / 60)
-
-  req_body_json(req, params)
+    req_throttle(CONFIG$rate_limit) |>
+    req_body_json(compact(params))
 }
 
 execute_request <- function(req) {
@@ -37,14 +64,11 @@ execute_request <- function(req) {
     resp_body_json()
 }
 
+create_date_query <- function(start_date, end_date) {
+  start <- as.Date(start_date)
+  end <- as.Date(end_date)
 
-# Patents -----------------------------------------------------------------
-
-build_patent_query <- function(start_date, end_date) {
-  assertDate(as.Date(start_date))
-  assertDate(as.Date(end_date))
-
-  if (as.Date(start_date) > as.Date(end_date)) {
+  if (start > end) {
     stop("start_date must be before or equal to end_date")
   }
 
@@ -60,35 +84,41 @@ build_patent_query <- function(start_date, end_date) {
   )
 }
 
+
+# API Functions -----------------------------------------------------------
 get_patents <- function(
-    fields = c("patent_id", "patent_title", "patent_date"),
     start_date = "2023-01-01",
     end_date = "2024-01-01",
+    fields = c("patent_id", "patent_title", "patent_date"),
     size = 10,
     api_key = get_api_key()) {
-  # Argument validation
-  assertNumber(size, lower = 1, upper = 1000, finite = TRUE)
+  # Validate size
+  if (size < 1 || size > CONFIG$max_page_size) {
+    stop(sprintf("size must be between 1 and %d", CONFIG$max_page_size))
+  }
 
-  query <- build_patent_query(start_date, end_date)
+  query <- create_date_query(start_date, end_date)
+  endpoint <- ENDPOINTS$patent
 
   req <- create_request(
-    "patent",
-    api_key,
+    endpoint$path,
     query = query,
-    fields = fields,
+    fields = fields %||% endpoint$default_fields,
     sort = list(list(patent_date = "asc")),
-    options = list(size = size)
+    options = list(size = size),
+    api_key = api_key
   )
 
   execute_request(req)
 }
 
 get_all_patents <- function(
-    fields = c("patent_id", "patent_title", "patent_date"),
     start_date = "2023-01-01",
     end_date = "2024-01-01",
+    fields = NULL,
     api_key = get_api_key()) {
-  query <- build_patent_query(start_date, end_date)
+  query <- create_date_query(start_date, end_date)
+  endpoint <- ENDPOINTS$patent
 
   next_req <- function(resp, req) {
     data <- resp_body_json(resp)
@@ -96,25 +126,25 @@ get_all_patents <- function(
       return(NULL)
     }
 
-    last_patent_id <- data$patents[[length(data$patents)]]$patent_id
+    last_id <- pluck(data$patents, -1, "patent_id")
 
     create_request(
-      "patent",
-      api_key,
+      endpoint$path,
       query = query,
-      fields = fields,
+      fields = fields %||% endpoint$default_fields,
       sort = list(list(patent_id = "asc")),
-      options = list(size = 1000, after = last_patent_id)
+      options = list(size = CONFIG$max_page_size, after = last_id),
+      api_key = api_key
     )
   }
 
   first_req <- create_request(
-    "patent",
-    api_key,
+    endpoint$path,
     query = query,
-    fields = fields,
+    fields = fields %||% endpoint$default_fields,
     sort = list(list(patent_id = "asc")),
-    options = list(size = 1000)
+    options = list(size = CONFIG$max_page_size),
+    api_key = api_key
   )
 
   resps <- req_perform_iterative(
@@ -129,62 +159,27 @@ get_all_patents <- function(
     resps_data(\(resp) resp_body_json(resp)$patents)
 }
 
-
-# IDs ---------------------------------------------------------------------
-
-configure_id_query <- function(
-    query_type = c("citations", "inventors", "locations"),
-    fields = NULL) {
-  endpoint_defaults <- list(
-    "citations" = list(
-      endpoint = "patent/us_patent_citation",
-      query_param = "citation_patent_id",
-      default_fields = c("patent_id", "citation_patent_id", "citation_category")
-    ),
-    "inventors" = list(
-      endpoint = "inventor",
-      query_param = "inventor_id",
-      default_fields = c("inventor_id", "inventor_lastknown_location")
-    ),
-    "locations" = list(
-      endpoint = "location",
-      query_param = "location_id",
-      default_fields = c("location_id", "location_name", "location_latitude", "location_longitude")
-    )
-  )
-
-  query_type <- match.arg(query_type)
-  config <- endpoint_defaults[[query_type]]
-
-  if (is.null(fields)) {
-    fields <- config$default_fields
-  }
-
-  list(
-    endpoint = config$endpoint,
-    query_param = config$query_param,
-    fields = fields
-  )
-}
-
-get_by_id <- function(
+get_related <- function(
     ids,
-    query_type = c("citations", "inventors", "locations"),
+    type = c("citations", "inventors", "locations"),
     fields = NULL,
     api_key = get_api_key()) {
   # Validate inputs
-  assertCharacter(ids, min.len = 1, unique = TRUE)
+  if (!is.character(ids) || length(ids) < 1 || any(duplicated(ids))) {
+    stop("Invalid IDs provided")
+  }
 
-  config <- configure_id_query(query_type, fields)
+  type <- match.arg(type)
+  endpoint <- ENDPOINTS[[type]]
 
-  # Create the request
+  query <- setNames(list(ids), endpoint$id_field)
+
   req <- create_request(
-    config$endpoint,
-    api_key,
-    query = setNames(list(ids), config$query_param),
-    fields = config$fields
+    endpoint$path,
+    query = query,
+    fields = fields %||% endpoint$default_fields,
+    api_key = api_key
   )
 
-  # Execute and return the request
   execute_request(req)
 }
--- main.R
+++ main.R
@@ -6,8 +6,7 @@ library(tidyr)
 # TODO: figure out how to not run out of memory
 #get_bulk_data(readRDS("references/bulk_data_urls.Rds"))
 
-
-# Calling API with functions ----------------------------------------------
+# Define fields for API requests
 api_fields <- c(
   "patent_id",
   "patent_date",
@@ -15,15 +14,20 @@ api_fields <- c(
   "inventors"
 )
 
-# Retrieve patents
-patents <- get_patents(fields = api_fields, size = 1000)
+# Retrieve a single page of patents
+patents <- get_patents(
+  fields = api_fields,
+  size = 1000
+)
 
 patents_tidy <- bind_rows(patents$patents) |>
   unnest_wider(col = inventors) |>
   mutate(inventor_id = basename(inventor))
 
-# Retrieve all patents
-all_patents <- get_all_patents(fields = api_fields)
+# Retrieve all patents using pagination
+all_patents <- get_all_patents(
+  fields = api_fields
+)
 
 all_patents_tidy <- bind_rows(all_patents) |>
   unnest_wider(col = inventors) |>
@@ -32,27 +36,27 @@ all_patents_tidy <- bind_rows(all_patents) |>
 # Verify unique patent count
 length(unique(all_patents_tidy$patent_id))
 
-# Retrieve citing patents using new get_by_id() function
-citing_patents <- get_by_id(
-  ids = unique(patents_tidy$patent_id), 
-  query_type = "citations"
+# Retrieve citing patents using new get_related() function
+citing_patents <- get_related(
+  ids = unique(patents_tidy$patent_id),
+  type = "citations"
 )
 
 citing_patents_tidy <- bind_rows(citing_patents$us_patent_citations)
 
-# Retrieve inventors using new get_by_id() function
-inventors <- get_by_id(
-  ids = unique(patents_tidy$inventor_id), 
-  query_type = "inventors"
+# Retrieve inventors using get_related() function
+inventors <- get_related(
+  ids = unique(patents_tidy$inventor_id),
+  type = "inventors"
 )
 
 inventors_tidy <- bind_rows(inventors$inventors) |>
   mutate(location_id = basename(inventor_lastknown_location))
 
-# Retrieve locations using new get_by_id() function
-locations <- get_by_id(
-  ids = unique(inventors_tidy$location_id), 
-  query_type = "locations"
+# Retrieve locations using get_related() function
+locations <- get_related(
+  ids = unique(inventors_tidy$location_id),
+  type = "locations"
 )
 
 locations_tidy <- bind_rows(locations$locations)
