684c3894401d9f2c0b3bf41016927bda4a3064c7
gabriellob0 <g.oliveiralobo@gmail.com>
2025-01-26 22:44:25 +0000

    Normalize line endings to LF

--- lse_diss/data/citations.py
+++ lse_diss/data/citations.py
@@ -1,4 +1,33 @@
+import pathlib
+import shutil
+
+import httpx
 import polars as pl
+from tqdm import tqdm
+
+
+def download_file(url):
+    path = pathlib.Path("data/raw")
+    path.mkdir(parents=True, exist_ok=True)
+    
+    file_name = url.split("/")[-1]
+    file_path = path / file_name 
+    
+    with open(file_path, "wb") as f:
+        with httpx.stream("GET", url) as r:
+            r.raise_for_status()
+            size = int(r.headers.get('content-length', 0))
+            with tqdm(total=size, unit='iB', unit_scale=True) as pbar:
+                for data in r.iter_bytes():
+                    pbar.update(len(data))
+                    f.write(data)
+    
+    shutil.unpack_archive(file_path, path)
+    file_path.unlink()
+
+download_file("https://s3.amazonaws.com/data.patentsview.org/download/g_cpc_title.tsv.zip")
+
+urls = pl.read_json("references/urls.json").to_dict()
 
 schema = {
     'patent_id': pl.Utf8,
