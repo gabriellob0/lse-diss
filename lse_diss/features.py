223608f12665914ae8c5086d0337d940cd4b8805
gabriellob0 <g.oliveiralobo@gmail.com>
2025-04-06 18:48:48 +0100

    feat: clean patent data

--- lse_diss/features.py
+++ lse_diss/features.py
@@ -2,11 +2,10 @@ from pathlib import Path
 
 import polars as pl
 
-patents_path = Path("data", "raw", "patents")
-
 
 def validate_patents(path):
-    df = pl.scan_parquet(patents_path, include_file_paths="file_name")
+    # TODO: validate missing and patent_date formatting
+    df = pl.scan_parquet(path, include_file_paths="file_name")
 
     dates = (
         df.select("patent_date", "patent_id", "file_name")
@@ -37,15 +36,86 @@ def validate_patents(path):
         .collect()
     )
 
-    print("date check: ", all([all(dates.get_column("min_test")), all(dates.get_column("max_test"))]))
+    print(
+        "date check: ",
+        all([all(dates.get_column("min_test")), all(dates.get_column("max_test"))]),
+    )
 
-    uniqueness = df.select("patent_id", "inventor_id", "inventor_location_id", "inventor_sequence").collect()
+    uniqueness = df.select(
+        "patent_id", "inventor_id", "inventor_location_id", "inventor_sequence"
+    ).collect()
     # NOTE: found a few duplicated inventors because of different locations for the same person
     # the rest is explained by inventor sequence
     duplicated = len(uniqueness.filter(uniqueness.is_duplicated())) == 0
     print("uniqueness check: ", duplicated)
 
-validate_patents(patents_path)
+
+def clean_patents(raw_path, clean_path):
+    years = [
+        "2000-01-01_to_2009-12-31",
+        "2010-01-01_to_2019-12-31",
+        "2020-01-01_to_2025-01-01",
+    ]
+
+    clean_path.mkdir(parents=True, exist_ok=True)
+
+    for year in years:
+        print("doing ", year)
+
+        path = clean_path / (year + ".parquet")
+
+        start_date, end_date = pl.Series(year.split("_to_")).str.to_date()
+
+        raw_patents = (
+            pl.scan_parquet(raw_path, low_memory=True)
+            .with_columns(
+                pl.col(
+                    ["patent_date", "patent_earliest_application_date"]
+                ).str.to_date()
+            )
+            .filter(pl.col("patent_date").is_between(start_date, end_date))
+        )
+
+        deduplicated_patents = (
+            raw_patents
+            .sort(
+                ["patent_id", "patent_date", "inventor_sequence"]
+            )
+            .unique(["patent_id", "inventor_id", "inventor_location_id"], keep="first")
+        )
+
+        inventor_locations = (
+            deduplicated_patents.with_columns(
+                pl.len().over("patent_id", "inventor_location_id").alias("count")
+            )
+            # rule 1
+            .filter(pl.col("count") == pl.max("count").over("patent_id"))
+            # rule 2
+            .filter(
+                pl.col("inventor_sequence")
+                == pl.min("inventor_sequence").over("patent_id")
+            )
+            .select("patent_id", "inventor_location_id")
+            .rename({"inventor_location_id": "patent_location_id"})
+        )
+
+        (
+            deduplicated_patents
+            .join(
+                inventor_locations, on="patent_id", how="left", validate="m:1"
+            )
+            .sink_parquet(path)
+        )
+
+
+data_path = Path("data")
+raw_patents_path = data_path / "raw" / "patents"
+clean_patents_path = data_path / "processed" / "patents"
+citations_path = data_path / "raw" / "bulk_downloads" / "g_us_patent_citation.parquet"
+
+validate_patents(raw_patents_path)
+clean_patents(raw_patents_path, clean_patents_path)
+
 
 # # path for bulk downloaded data
 # base_path = Path("data", "raw", "bulk_downloads")
