95acbce6662fccd53f24780ff8aaa3c613277a28
gabriellob0 <g.oliveiralobo@gmail.com>
2025-04-09 18:23:17 +0100

    fix: issues w renv

--- main.R
+++ main.R
@@ -1,8 +1,12 @@
 # Imports ----
+library(reticulate)
+use_virtualenv(here::here(".venv"))
+
+features <- import("lse_diss.features")
 
 source("lse_diss/data/make_client.R")
 source("lse_diss/data/make_data.R")
 
 # Data ----
-client <- make_client()
-make_data(client)
+#client <- make_client()
+#make_data(client)
