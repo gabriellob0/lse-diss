311d83cc3196e11218a9eb8a553c2de48371789a
gabriellob0 <g.oliveiralobo@gmail.com>
2025-04-11 17:03:50 +0100

    feat: add memory to benchmark

--- lse_diss/modelling/benchmark.py
+++ lse_diss/modelling/benchmark.py
@@ -1,18 +1,36 @@
+import os
 import time
 from pathlib import Path
 
 import polars as pl
+import psutil
 from sentence_transformers import SentenceTransformer
 
-start_time = time.time()
 
-size = 20000
+def print_memory_usage():
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    print(f"Memory usage: {mem_info.rss / (1024 * 1024):.2f} MB")
+
+
+print("cpu count")
+psutil.cpu_count()
+
+print_memory_usage()
+
+size = 5000
 
 model = SentenceTransformer(
     "nomic-ai/nomic-embed-text-v2-moe", trust_remote_code=True, truncate_dim=256
 )
 
-patents = pl.scan_parquet(Path("data", "interim", "abstracts.parquet")).slice(0, size).collect()
+start_time = time.time()
+
+patents = (
+    pl.scan_parquet(Path("data", "interim", "abstracts.parquet"))
+    .slice(0, size)
+    .collect()
+)
 
 abstracts = patents.get_column("patent_abstract").to_list()
 
@@ -20,4 +38,6 @@ embeddings = model.encode(abstracts, prompt_name="passage", show_progress_bar=Tr
 
 finish_time = time.time()
 
-print(finish_time - start_time)
+print_memory_usage()
+
+print("total time: ", finish_time - start_time)
