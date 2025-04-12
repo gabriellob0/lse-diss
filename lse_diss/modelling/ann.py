8f9d0403f7bf96a16444a7c9db92c0c7a90914d4
gabriellob0 <g.oliveiralobo@gmail.com>
2025-04-12 19:08:10 +0100

    fix: use control fn with join instead of filter

--- lse_diss/modelling/ann.py
+++ lse_diss/modelling/ann.py
@@ -43,64 +43,7 @@ def match_controls(
     n_neighbours = (
         patents_with_embeddings.unique("patent_id")
         # NOTE: this is where the top 1% is defined
-        .select(pl.len() / 10000)
-        .collect()
-        .item(0, 0)
-    )
-
-    print("neighbours: ", ceil(n_neighbours))
-
-    embeddings = (
-        patents_with_embeddings.select("embedding")
-        .collect()
-        .get_column("embedding")
-        .to_numpy()
-    )
-
-    print("querying NNs")
-
-    neighbours, _ = voyager_index.query(embeddings, ceil(n_neighbours))
-
-    patents_with_neighbours = (
-        patents_with_embeddings.select("patent_id")
-        .with_columns(neighbours=neighbours)
-        .explode(pl.col("neighbours"))
-        .rename({"neighbours": "neighbour"})
-    )
-
-    controls = (
-        raw_controls.join(
-            patents_with_neighbours, left_on="citing_patent_id", right_on="patent_id"
-        )
-        .join(
-            patents_with_embeddings.select(["patent_id", "voyager_index"]),
-            left_on="control_patent_id",
-            right_on="patent_id",
-        )
-        .filter(pl.col("voyager_index") == pl.col("neighbour"))
-        .select(["citing_patent_id", "cited_patent_id", "control_patent_id"])
-    )
-
-    print("sinking data")
-
-    controls.sink_parquet(save_path)
-
-
-def match_controls2(
-    voyager_index,
-    embeddings_path=Path("data", "processed", "embeddings"),
-    controls_path=Path("data", "interim", "controls"),
-    save_path=Path("data", "processed", "controls.parquet"),
-):
-    patents_with_embeddings = pl.scan_parquet(embeddings_path).with_row_index(
-        "voyager_index"
-    )
-    raw_controls = pl.scan_parquet(controls_path)
-
-    n_neighbours = (
-        patents_with_embeddings.unique("patent_id")
-        # NOTE: this is where the top 1% is defined
-        .select(pl.len() / 10000)
+        .select(pl.len() / 100)
         .collect()
         .item(0, 0)
     )
@@ -136,6 +79,7 @@ def match_controls2(
             patents_with_neighbours,
             on=["citing_patent_id", "voyager_index"]
         )
+        .select(["citing_patent_id", "cited_patent_id", "control_patent_id"])
     )
 
     print("sinking data")
@@ -145,4 +89,4 @@ def match_controls2(
 
 
 #create_index()
-match_controls2(open_index())
+match_controls(open_index())
