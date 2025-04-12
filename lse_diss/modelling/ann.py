401d388531958bbc45cb37006eedb5ae6a1cb40e
gabriellob0 <g.oliveiralobo@gmail.com>
2025-04-12 15:15:55 +0100

    feat: add final matching fn

--- lse_diss/modelling/ann.py
+++ lse_diss/modelling/ann.py
@@ -1,10 +1,14 @@
+from math import ceil
 from pathlib import Path
 
 import polars as pl
-
 from voyager import Index, Space
 
-def create_index(embeddings_path=Path("data", "processed", "embeddings"), save_path=Path("data", "interim", "indexes")):
+
+def create_index(
+    embeddings_path=Path("data", "processed", "embeddings"),
+    save_path=Path("data", "interim", "indexes"),
+):
     save_path.mkdir(parents=True, exist_ok=True)
     file_path = str(save_path / "index.voy")
 
@@ -17,29 +21,56 @@ def create_index(embeddings_path=Path("data", "processed", "embeddings"), save_p
 
     index.save(str(file_path))
 
-#create_index()
 
-with open(str(Path("data", "interim", "indexes", "index.voy")), 'rb') as f:
-    index = Index.load(f) 
-
-index.num_elements
-
-# plan:
-# filter all citing patents
-# for each citing patent get top 1% nearest neighbour
-# construct dataset with these
-# join on citing and control id from potential control
-
-# after that:
-# go back to features
-# join all the locations
-# calculate the distances (between citing and cited and control and cited)
-# move to data analysis
+def open_index(path=Path("data", "interim", "indexes", "index.voy")):
+    path_str = str(path)
+    with open(path_str, "rb") as f:
+        index = Index.load(f)
+    return index
 
 
+def match_controls(
+    voyager_index,
+    controls_path=Path("data", "interim", "controls"),
+    embeddings_path=Path("data", "processed", "embeddings"),
+    save_path=Path("data", "processed", "controls.parquet"),
+):
+    potential_controls = pl.scan_parquet(controls_path)
+    indexed_embeddings = pl.scan_parquet(embeddings_path)
 
-#neighbors, distances = index.query(embeddings[5], k=3)
+    citing_ids = potential_controls.select("citing_patent_id").unique()
 
-#abstracts[5]
-#abstracts[17281]
-#abstracts[75715]
+    n_citing = indexed_embeddings.select(pl.len()).collect().item(0, 0)
+    n_neighbours = ceil(n_citing / 100)
+    print(n_neighbours)
+
+    citing_embeddings = citing_ids.join(
+        indexed_embeddings.select(["patent_id", "embedding"]),
+        left_on="citing_patent_id",
+        right_on="patent_id",
+        how="left",
+        validate="1:1",
+    )
+
+    embeddings = citing_embeddings.collect().get_column("embedding").to_numpy()
+
+    neighbours, _ = voyager_index.query(embeddings, n_neighbours)
+
+    citing_neighbours = (
+        citing_ids.with_columns(neighbour=neighbours)
+        .explode(pl.col("neighbour"))
+        .join(
+            indexed_embeddings.select(["index", "patent_id"]),
+            left_on="neighbour",
+            right_on="index",
+        )
+        .select(
+            pl.col("citing_patent_id"), pl.col("patent_id").alias("control_patent_id")
+        )
+    )
+
+    controls = potential_controls.join(
+        citing_neighbours, on=["citing_patent_id", "control_patent_id"]
+    )
+
+    controls.sink_parquet(save_path)
