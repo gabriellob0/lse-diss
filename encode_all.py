2f1e87b87fda5ce076168f841c3f703f4d589203
gabriellob0 <g.oliveiralobo@gmail.com>
2025-04-15 08:50:15 +0100

    feat: encode all to handle previously created

--- encode_all.py
+++ encode_all.py
@@ -6,21 +6,38 @@ from lse_diss.modelling.embeddings import make_embeddings
 
 
 def encode_all():
+    patents_path = Path("data", "interim", "patents")
     abstracts_path = Path("data", "misc", "abstracts")
-    abstracts_path.mkdir(parents=True, exist_ok=True)
+    embeddings_path = Path("data", "misc", "embeddings")
 
-    (
-        pl.scan_parquet(Path("data", "interim", "patents"))
-        .select("patent_id", "patent_abstract")
-        .unique()
-        .sink_parquet(
+    patents = (
+        pl.scan_parquet(patents_path).select(["patent_id", "patent_abstract"]).unique()
+    )
+
+    if embeddings_path.exists():
+        [file_path.unlink() for file_path in abstracts_path.glob('*')]
+        
+        embeddings = pl.scan_parquet(embeddings_path).select("patent_id")
+        patents.join(embeddings, on="patent_id", how="anti").sink_parquet(
             pl.PartitionMaxSize(
                 abstracts_path / "abstract_{part}.parquet", max_size=512_000
             )
         )
-    )
+    else:
+        abstracts_path.mkdir(parents=True, exist_ok=True)
 
-    make_embeddings(abstracts_path, Path("data", "misc", "embeddings"))
+        (
+            pl.scan_parquet(Path("data", "interim", "patents"))
+            .select("patent_id", "patent_abstract")
+            .unique()
+            .sink_parquet(
+                pl.PartitionMaxSize(
+                    abstracts_path / "abstract_{part}.parquet", max_size=512_000
+                )
+            )
+        )
+
+    make_embeddings(abstracts_path, embeddings_path)
 
 
 if __name__ == "__main__":
