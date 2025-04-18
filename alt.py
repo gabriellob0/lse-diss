b2d594d75b9135890a646c63d6109011cc745a17
lobodeol <lobodeol@medium-dy-m7a-2xlarge-1.fabian-20240520-1545.pcluster>
2025-04-18 16:01:10 +0000

    fix: comment done part out

--- alt.py
+++ alt.py
@@ -2,19 +2,22 @@ from pathlib import Path
 
 from lse_diss.features.controls import make_originating, make_treated, save_controls
 from lse_diss.features.abstracts import filter_embeddings
+from lse_diss.modelling.ann import create_index, open_index, match_controls
 
-BASE_YEAR = 5
+#BASE_YEAR = 2005
 
-originating = make_originating(base_year=BASE_YEAR, duration_months=0, duration_years=5)
+#originating = make_originating(base_year=BASE_YEAR, duration_months=0, duration_years=1)
 
-treated = make_treated(originating, base_year=BASE_YEAR, duration=20)
+#treated = make_treated(originating, base_year=BASE_YEAR, duration=5)
 
-save_controls(
-    originating,
-    treated,
-    duration=20,
-    search_range=180,
-    path=Path("data", "interim", "controls_alt"),
-)
+#save_controls(
+#    originating,
+#    treated,
+#    duration=5,
+#    search_range=180,
+#    path=Path("data", "interim", "controls_alt"),
+#    batch_size=1000
+#)
 
-filter_embeddings()
+#filter_embeddings()
+
